10585e9e7b0dea5e9da69217d3dd31a8b34c5a2d
Christopher Kim <chris.kim1990@gmail.com>
2020-09-23 14:44:58 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,18 +17,10 @@ def input_to_index(user_input)
 end
 
 def valid_move?(board, index)
-  board = index.to_i - 1
-    if !position_taken?(board, index) && index.between?(0,8)
-      true
+  def position_taken?(board, index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+      return false
     else
-      false
+      return true
     end
-end
-
-def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    false
-  else
-    true
-  end
 end

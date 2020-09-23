92efbab5e7946bded55816c31945701258341bba
Christopher Kim <chris.kim1990@gmail.com>
2020-09-23 15:14:18 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,6 +8,17 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
+  num = gets.chomp
+  index = input_to_index(num)
+  if valid_move?(board, index) == true
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+
 end
 
 def input_to_index(user_input)
